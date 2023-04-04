5b87b341132ac34280607c951dda787487251544
Simen E. Sørensen <simen_sor91@hotmail.com>
2023-04-04 10:05:09 +0200

    Set WiFi credentials via Serial insead of header

--- OpenMacroRail_Arduino/OpenMacroRail_Arduino.ino
+++ OpenMacroRail_Arduino/OpenMacroRail_Arduino.ino
@@ -1,10 +1,10 @@
 #include <WiFiClient.h>
 #include <WiFi.h>
+#include <esp_wifi.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <SpeedyStepper.h>
 #include <Preferences.h>
-#include "WiFi-credentials.h"
 
 // Unused pin declarations (these pins are present on Motorized Macro Slider PCB V1.2):
 // const int LIMIT1_PIN = 12;
@@ -115,6 +115,58 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
+}
+
+bool connectWifi(String ssid, String pass)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), pass.c_str());
+
+  int retry = 0;
+  while (retry < 10 && WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    retry++;
+    delay(500);
+  }
+
+  return WiFi.status() == WL_CONNECTED;
+}
+
+bool autoConnectToWifi()
+{
+  String ssid;
+  String pass;
+
+  wifi_config_t conf;
+  esp_wifi_get_config(WIFI_IF_STA, &conf);
+
+  ssid = String(reinterpret_cast<const char *>(conf.sta.ssid));
+  pass = String(reinterpret_cast<char *>(conf.sta.password));
+
+  Serial.println("autoConnectToWifi");
+  Serial.println(ssid);
+  Serial.println(pass);
+
+  bool connected = false;
+  // Check if the ESP auto connected
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    connected = true;
+  }
+
+  // If it didn't auto connect, try connect with the stored
+  // details
+  if (connected || connectWifi(ssid, pass))
+  {
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    return true; // connected success
+  }
+
+  return false;
 }
 
 void jog_fwd(){
@@ -251,26 +303,50 @@ void setup(void){
     
     ledcWrite(LED_CHANNEL, 0);
   }
-
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
   
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
   Serial.println("");
 
   preferences.begin("settings",false);
   readSettingsFromEEPROM();
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  //AutoConnect will try connect to Wifi using details stored in flash
+  if (!autoConnectToWifi())
+  {
+    bool exitWifiConnect = false;
+    String ssid = "";
+    String pass = "";
+    int stage = 0;
+    Serial.setTimeout(20000); //20 seconds
+    //Wait here til we get Wifi details
+    while (!exitWifiConnect)
+    {
+
+      Serial.println("Please enter your Wifi SSID:");
+      ssid = Serial.readStringUntil('\r'); // Web Flash tools use '\r\n' for line endings
+      Serial.read();
+      if (ssid != "")
+      {
+        Serial.println("Please enter your Wifi password:");
+        pass = Serial.readStringUntil('\r');
+        Serial.read();
+        Serial.print("Connecting...");
+        exitWifiConnect = connectWifi(ssid, pass);
+      }
+      if (!exitWifiConnect)
+      {
+        Serial.println("Failed to connect.");
+      }
+      else
+      {
+        Serial.println("Connected!");  
+        
+        Serial.print("\nWiFi connected to: ");
+        Serial.println(ssid);
+      }
+    }
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
+
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
